b208c48de171c807d39f452253fab83a2dd1be95
Tiago Rafael Oliveira das Virgens <tiago.rafael@compasso.com.br>
2023-12-22 13:24:13 -0400

    feat: firsts endpoints of user

---
+++ src/user/user.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { UserRepository } from "./user.repository";
+
+@Controller("/users")
+export class UserController {
+
+    constructor(private userRepository: UserRepository) {}
+
+    @Post()
+    async createUser(@Body() userData) {
+        this.userRepository.save(userData);
+        return userData;
+    }
+
+    @Get()
+    async listUsers() {
+        return this.userRepository.list();
+    }
+}
---
+++ src/user/user.repository.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class UserRepository {
+    private users = [];
+
+    async save(user) {
+        this.users.push(user);
+    }
+
+    async list() {
+        return this.users;
+    }
+}
